ebff3006db2bd3b613ee3c00ba162bbbe1e91bf0
Remo Giermann <mo@liberejo.de>
2012-03-31 09:17:39 +0200

    minor fix

--- code/main.c
+++ code/main.c
@@ -16,7 +16,44 @@ static struct {
 	ctrlio_t uinit;
 } conf[LOOPS] = {{0,MAX,0,0}};
 
-static void init();
+static void init()
+{
+    uint32_t div;
+    uint32_t tcclks;
+
+    /* Enable peripheral clock. */
+    PMC->PMC_PCER0 = 1 << ID_TC1;
+
+    /* Configure TC */
+    TC_FindMckDivisor(SAMPLING_FREQ, BOARD_MCK, &div, &tcclks, BOARD_MCK);
+    TC_Configure(TC1, BLINK_TC, tcclks | TC_CMR_CPCTRG);
+    TC1->TC_CHANNEL[0].TC_RC = (BOARD_MCK/div) / SAMPLING_FREQ;
+
+    /* Configure and enable interrupt on RC compare */
+    NVIC_EnableIRQ((IRQn_Type) ID_TC1);
+    TC1->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
+
+	/* Tick Config */
+	TimeTick_Configure(BOARD_MCK);
+
+    /* Initialize ADC */
+    ADC_Initialize( ADC, ID_ADC );
+
+    /* startup = 15, prescal = 4, ADC clock = 6.4 MHz */
+    ADC_cfgFrequency( ADC, 15, 4 );
+
+    /* Enable ADC interrupt */
+    NVIC_EnableIRQ(ADC_IRQn);
+    ADC->ADC_IER = ADC_EOC_IRQS;
+
+	/* LED PIO Config */
+	LEDs_configure();
+	LED_clr(statusled);
+
+	/* USB Console Config */
+	USBC_Configure();
+
+}
 
 int main() 
 {
@@ -182,42 +219,4 @@ void ADC_IrqHandler(void)
 	}
 }
 
-void init()
-{
-    uint32_t div;
-    uint32_t tcclks;
-
-    /* Enable peripheral clock. */
-    PMC->PMC_PCER0 = 1 << ID_TC1;
-
-    /* Configure TC */
-    TC_FindMckDivisor(SAMPLING_FREQ, BOARD_MCK, &div, &tcclks, BOARD_MCK);
-    TC_Configure(TC1, BLINK_TC, tcclks | TC_CMR_CPCTRG);
-    TC1->TC_CHANNEL[0].TC_RC = (BOARD_MCK/div) / SAMPLING_FREQ;
-
-    /* Configure and enable interrupt on RC compare */
-    NVIC_EnableIRQ((IRQn_Type) ID_TC1);
-    TC1->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
-
-	/* Tick Config */
-	TimeTick_Configure(BOARD_MCK);
-
-    /* Initialize ADC */
-    ADC_Initialize( ADC, ID_ADC );
-
-    /* startup = 15, prescal = 4, ADC clock = 6.4 MHz */
-    ADC_cfgFrequency( ADC, 15, 4 );
-
-    /* Enable ADC interrupt */
-    NVIC_EnableIRQ(ADC_IRQn);
-    ADC->ADC_IER = ADC_EOC_IRQS;
-
-	/* LED PIO Config */
-	LEDs_configure();
-	LED_clr(statusled);
-
-	/* USB Console Config */
-	USBC_Configure();
-
-}
 /* vim: set ts=4: */
