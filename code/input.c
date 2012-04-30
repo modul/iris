83717280c430fcf8e13911f9cf0a5dcda77c611c
Remo Giermann <mo@liberejo.de>
2012-04-30 13:47:42 +0200

    converting current, previous readings to millivolts

--- code/input.c
+++ code/input.c
@@ -1,13 +1,14 @@
 #include <string.h>
 #include "conf.h"
 
-#define VOLT(b) ((b*VREF)>>RESOLUTION)
 #define TIMER_FREQ (NUM_AIN * SAMPLING_FREQ)
+
+#define mV(b) ((b*VREF)>>RESOLUTION)
 
 static uint16_t next[NUM_AIN] = {0};
 
-uint16_t current[NUM_AIN] = {0};
-uint16_t previous[NUM_AIN] = {0};
+uint16_t current[NUM_AIN] = {0};   // current ADC input in mV
+uint16_t previous[NUM_AIN] = {0};  // previous ADC input in mV
 
 void TC0_IrqHandler()
 {
@@ -19,6 +20,7 @@ void TC0_IrqHandler()
 
 void ADC_IrqHandler()
 {
+	uint8_t i;
     uint32_t status;
 	static uint32_t timestamp = 0;
 
@@ -26,9 +28,10 @@ void ADC_IrqHandler()
 
 	if ((status & ADC_ISR_RXBUFF) == ADC_ISR_RXBUFF) {
 		memcpy(previous, current, NUM_AIN*2);
-		memcpy(current, next, NUM_AIN*2);
+		for (i=0; i<NUM_AIN; i++)
+			current[i] = mV(next[i]);
 
-		TRACE_DEBUG("[%u] Got samples. 0: %umV, 1: %umV, 2: %umV\n", GetTickCount()-timestamp, VOLT(current[0]), VOLT(current[1]), VOLT(current[2]));
+		TRACE_DEBUG("[%u] Got samples. 0: %umV, 1: %umV, 2: %umV\n", GetTickCount()-timestamp, current[0], current[1], current[2]);
 		timestamp = GetTickCount();
 
 		ADC_ReadBuffer(ADC, (int16_t*) next, NUM_AIN);
