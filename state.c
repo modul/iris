87c8ff9752cbef70675e972f08a4df8bb18a8562
Remo Giermann <mo@liberejo.de>
2012-05-25 03:32:53 +0200

    minor cleanup

--- state.c
+++ state.c
@@ -20,17 +20,40 @@ static void do_error();
 static void do_conf();
 static void do_ack();
 
+static unsigned _state = IDLE;
+static uint16_t soffset = 0;
+
 static struct state actions[NUMSTATES][NUMEVENTS] = {
-/* events      {EV_START, EV_ABORT, EV_LOG, EV_CONF, EV_ESTOP, EV_PTRIG, EV_FTRIG};*/
+/* event/state EV_START,          EV_ABORT,         EV_LOG,          EV_CONF,          EV_ESTOP,          EV_PTRIG,          EV_FTRIG       */
 /* IDLE  */  {{do_press, READY}, {do_abort, IDLE}, {do_log,  IDLE}, {do_conf,  IDLE}, {do_error, ERROR}, {   NULL,   IDLE}, {   NULL,  IDLE}},
 /* READY */  {{  do_nok, READY}, {do_abort, IDLE}, {do_log, READY}, { do_nok, READY}, {do_error, ERROR}, {do_stop,    SET}, {   NULL,  READY}},
 /* SET   */  {{do_press,    GO}, {do_abort, IDLE}, {do_log,   SET}, { do_nok,   SET}, {do_error, ERROR}, {   NULL,    SET}, {   NULL,  SET}},
 /* GO    */  {{  do_nok,    GO}, {do_abort, IDLE}, {do_log,    GO}, { do_nok,    GO}, {do_error, ERROR}, {   NULL,     GO}, {do_vent,  IDLE}},
 /* ERROR */  {{  do_nok, ERROR}, {  do_ack, IDLE}, {do_log, ERROR}, { do_nok, ERROR}, {do_error, ERROR}, {   NULL,  ERROR}, {   NULL,  ERROR}},
+/*              action    next                                                                                                               */
 };
 
-static unsigned _state = IDLE;
-static uint16_t soffset = 0;
+void send_event(unsigned ev)
+{
+	paction_t action = NULL;
+
+	assert (ev < NUMEVENTS);
+	action = actions[_state][ev].action;
+	if (action)
+		action();
+	_state = actions[_state][ev].next;
+}
+
+void reset_state()
+{
+	do_vent();
+	_state = IDLE;
+}
+
+unsigned get_state()
+{
+	return _state;
+}
 
 static void do_nok()
 {
@@ -46,7 +69,7 @@ static void do_abort()
 static void do_ack()
 {
 	// check emergency stop here
-	LED_on(ALARM);
+	LED_off(ALARM);
 	do_vent();
 }
 
@@ -62,13 +85,13 @@ static void do_conf()
 	char c;
 	if (scanf("%u %u %u %u", &cnf.fmax, &cnf.pmax, &cnf.smax, &cnf.gainid) == 4) {
 		set_config(cnf);
-		printf("ok %u %u %u %u\n", cnf.fmax, cnf.pmax, cnf.smax, cnf.gainid);
+		printf("ok ");
 	}
 	else {
 		get_config(&cnf);
-		printf("%u %u %u %u\n", cnf.fmax, cnf.pmax, cnf.smax, cnf.gainid);
 	}
 
+	printf("%u %u %u %u\n", cnf.fmax, cnf.pmax, cnf.smax, cnf.gainid);
 	while ((c = getchar()) != 10 || c != 0);
 }
 
@@ -98,24 +121,4 @@ static void do_vent()
 	PIO_Clear(&pr); PIO_Set(&vn);
 }
 
-void send_event(unsigned ev)
-{
-	paction_t action = NULL;
 
-	assert (ev < NUMEVENTS);
-	action = actions[_state][ev].action;
-	if (action)
-		action();
-	_state = actions[_state][ev].next;
-}
-
-void reset_state()
-{
-	do_vent();
-	_state = IDLE;
-}
-
-unsigned get_state()
-{
-	return _state;
-}
