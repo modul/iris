76180e38a8682f0c656635a59efef187c36f54bc
Remo Giermann <mo@liberejo.de>
2012-07-10 12:54:40 +0200

    doing a clean startup in start_sampling
    initially starting a new conversion on the current channel there

--- input.c
+++ input.c
@@ -2,6 +2,7 @@
 #include "input.h"
 #include "ad7793.h"
 
+static int next = 0;
 static int latest[NUM_AIN] = {0};  
 static int previous[NUM_AIN] = {0}; 
 
@@ -17,31 +18,31 @@ static struct chan channel[NUM_AIN] = {
 void TC0_IrqHandler()
 {
 	uint32_t status = TC0->TC_CHANNEL[0].TC_SR;
-	static uint8_t n = 0;
 
-	TRACE_DEBUG("n=%u\n", n);
+	TRACE_DEBUG("n=%u\n", next);
 
 	if ((status = ain_status()) & AD_STAT_NRDY) {
-		TRACE_INFO("ADC ch%u not ready (%x)\n", channel[n].num, status);
+		TRACE_INFO("ADC ch%u not ready (%x)\n", channel[next].num, status);
 		return; // try again next time
 	}
 	else if (status & AD_STAT_ERR) {
-		TRACE_ERROR("ADC error ch%u (%x)\n", channel[n].num, status);
+		TRACE_ERROR("ADC error ch%u (%x)\n", channel[next].num, status);
 	}
 	else {
-		previous[n] = latest[n];
-		latest[n] = ain_read();
-		TRACE_DEBUG("ADC read %umV (%x)\n", mv(latest[n]), status);
+		previous[next] = latest[next];
+		latest[next] = ain_read();
+		TRACE_DEBUG("ADC read %umV (%x)\n", mv(latest[next]), status);
 
-		if (++n == NUM_AIN)
-			n = 0;
+		if (++next == NUM_AIN)
+			next = 0;
 	}
 
-	ain_start(channel[n].num, channel[n].gain, AD_MODE_SINGLE);
+	ain_start(channel[next].num, channel[next].gain, AD_MODE_SINGLE);
 }
 
 void start_sampling()
 {
+	ain_start(channel[next].num, channel[next].gain, AD_MODE_SINGLE);
 	NVIC_EnableIRQ(TC0_IRQn);
 	NVIC_SetPriority(TC0_IRQn, 1);
 }
