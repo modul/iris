5ef65a28d8b002e8f67d2c18a1340eb675d408cb
Remo Giermann <mo@liberejo.de>
2012-08-07 05:04:19 +0200

    named the board IRIS, cleaned up board.h

--- board/board.h
+++ board/board.h
@@ -14,42 +14,41 @@
 #define LEDS_ACTIVE_LOW
 #include "led.h"
 
-/** Name of the board */
-#define BOARD_NAME "SAM3-H256"
-/** Board definition */
-#define sam3sek
-/** Family definition (already defined) */
+/* Name of the board */
+#define BOARD_NAME "IRIS"
+
+/* Family definition (already defined) */
 #define sam3s
-/** Core definition */
+/* Core definition */
 #define cortexm3
 
 #define BOARD_MAINOSC 12000000
 #define BOARD_MCK     48000000
 
-/** LED pins **/
+/* LED pins **/
 #define PIN_STATUS {PIO_PA8, PIOA, ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
 #define PIN_ALARM {PIO_PA31, PIOA, ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT}
 #define PINS_LEDS PIN_STATUS, PIN_ALARM
 
-/** Blinker configuration (LED Flashing) **/
+/* Blinker configuration (LED Flashing) **/
 #define LEDS_NUM 2
 #define STATUS 0
 #define ALARM 1
 #define BLINK_TIME 100
 
-/** USB configuration **/
+/* USB configuration **/
 #define PIN_USB_VBUS    {PIO_PA24, PIOA, ID_PIOA, PIO_INPUT, PIO_PULLUP}
 
-/** USB attributes configuration descriptor (bus or self powered, remote wakeup) */
+/* USB attributes configuration descriptor (bus or self powered, remote wakeup) */
 #define BOARD_USB_BMATTRIBUTES  USBConfigurationDescriptor_SELFPOWERED_RWAKEUP
 
-/** DBGU configuration (UART console) **/
+/* DBGU configuration (UART console) **/
 #define UART_BAUDRATE 115200
 #define UART_UART     UART0
 #define UART_ID       ID_UART0
 #define PINS_UART  { PIO_PA9A_URXD0|PIO_PA10A_UTXD0, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
 
-/** standard IO configuration for syscalls **/
+/* standard IO configuration for syscalls **/
 #define read_stdin   USBC_Gets
 #define write_stdout USBC_Puts
 #define read_stderr  UART_Gets // For convenience, stderr is used both ways
