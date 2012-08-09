63a19b8120245fc8293cfe749316337bde139d0c
Remo Giermann <mo@liberejo.de>
2012-08-09 19:35:21 +0200

    flash storrage kinda working with structured data

--- flashit.c
+++ flashit.c
@@ -4,16 +4,16 @@
 #define LASTPAGE (IFLASH_ADDR + IFLASH_SIZE - IFLASH_PAGE_SIZE)
 #define LASTPAGENO ((LASTPAGE - IFLASH_ADDR)/IFLASH_PAGE_SIZE)
 
-int channels[3] = {-1, 42, 1024};
+struct c {
+	int min;
+	int max;
+} chnls[3] = {{-1, 1}, {0, 1024}, {-1024, 42}};
 
 void flashread()
 {
-	volatile uint32_t *buffer = (volatile uint32_t *) LASTPAGE;
-	TRACE_DEBUG("%i\n", *buffer);
-	buffer++;
-	TRACE_DEBUG("%i\n", *buffer);
-	buffer++;
-	TRACE_DEBUG("%i\n", *buffer);
+	uint32_t *data = (uint32_t *) LASTPAGE;
+	
+	TRACE_DEBUG("%i %i %i %i %i %i\n", data[0], data[1], data[2], data[3], data[4], data[5]);
 }
 
 void FlashRam_WriteCommand() __attribute__((section(".data")));
@@ -23,7 +23,7 @@ void FlashRam_WriteCommand()
 	const int KEY = 0x5A << 24;
 	uint32_t i = 0;
 	uint32_t *page = (uint32_t *) LASTPAGE;
-	uint32_t *data = (uint32_t *) channels;
+	uint32_t *data = (uint32_t *) chnls;
 	
 	for (i=0; i < IFLASH_PAGE_SIZE/4; i++) {
 		*page++ = *data++;
